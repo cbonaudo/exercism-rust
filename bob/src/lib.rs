32bd1348098df474434c206dbd377b49ed83314f
cbonaudo <cbonaudo@hotmail.fr>
2021-06-16 21:13:12 +0200

    last easy

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,3 +1,22 @@
 pub fn reply(message: &str) -> &str {
-    unimplemented!("have Bob reply to the incoming message: {}", message)
+    if message.trim().len() == 0 {
+        "Fine. Be that way!"
+    } else if message
+        .chars()
+        .filter(|x| x.is_ascii_alphabetic() && x.is_ascii_lowercase())
+        .collect::<String>()
+        .len()
+        == 0
+        && message.chars().any(|x| x.is_ascii_uppercase())
+    {
+        if message.trim_end().chars().last() == Some('?') {
+            "Calm down, I know what I'm doing!"
+        } else {
+            "Whoa, chill out!"
+        }
+    } else if message.trim_end().chars().last() == Some('?') {
+        "Sure."
+    } else {
+        "Whatever."
+    } 
 }
--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -1,6 +1,39 @@
+fn has_corresponding(c: char, temp: &Vec<char>) -> bool {
+    if let Some(last_char) = temp.iter().last() {
+        *last_char == c
+    } else {
+        false
+    }
+}
+
+fn remove_corresponding(c: char, temp: &mut Vec<char>) {
+    let index = temp.iter().rev().position(|x| *x == c).unwrap();
+    temp.remove(temp.len() - index - 1);
+}
+
 pub fn brackets_are_balanced(string: &str) -> bool {
-    unimplemented!(
-        "Check if the string \"{}\" contains balanced brackets",
-        string
-    );
+    let mut temp: Vec<char> = Vec::new();
+
+    for c in string.chars().collect::<Vec<char>>() {
+        if c == '[' || c == '{' || c == '(' {
+            temp.push(c);
+        }
+
+        if c == ']' || c == '}' || c == ')' {
+            let corres = match c {
+                ']' => '[',
+                '}' => '{',
+                ')' => '(',
+                _ => ' ',
+            };
+
+            if has_corresponding(corres, &temp) {
+                remove_corresponding(corres, &mut temp);
+            } else {
+                return false;
+            }
+        }
+    }
+
+    temp.len() == 0 
 }
