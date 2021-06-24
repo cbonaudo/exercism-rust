08c362c857c6fa994ca04ce9db13e614f3b47e0f
cbonaudo <cbonaudo@hotmail.fr>
2021-06-24 11:58:50 +0200

    nucleo

--- nucleotide-count/src/lib.rs
+++ nucleotide-count/src/lib.rs
@@ -1,16 +1,27 @@
 use std::collections::HashMap;
 
+static NUC_CHAR: [char; 4] = ['A', 'T', 'G', 'C'];
+
 pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
-    unimplemented!(
-        "How much of nucleotide type '{}' is contained inside DNA string '{}'?",
-        nucleotide,
-        dna
-    );
+    NUC_CHAR
+        .iter()
+        .find(|c| **c == nucleotide)
+        .ok_or(nucleotide)?;
+
+    dna.chars()
+        .map(|f| NUC_CHAR.iter().find(|c| **c == f).ok_or(f))
+        .collect::<Result<Vec<&char>, char>>()?;
+
+    Ok(dna.chars().filter(|c| *c == nucleotide).count())
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
-    unimplemented!(
-        "How much of every nucleotide type is contained inside DNA string '{}'?",
-        dna
-    );
+    let mut hashmap = HashMap::new();
+
+    NUC_CHAR.iter().try_for_each(|c| -> Result<(), char> {
+        hashmap.insert(*c, count(c.clone(), dna)?);
+        Ok(())
+    })?;
+
+    Ok(hashmap)
 }
