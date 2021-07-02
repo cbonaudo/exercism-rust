4fe04d08f977f4afc6476909372f0a68a84f795d
cbonaudo.externe <cbonaudo.externe@bedrockstreaming.com>
2021-07-02 15:32:36 +0200

    robot simu

--- robot-simulator/src/lib.rs
+++ robot-simulator/src/lib.rs
@@ -9,37 +9,76 @@ pub enum Direction {
     West,
 }
 
-pub struct Robot;
+pub struct Robot {
+    position: (i32, i32),
+    direction: Direction,
+}
 
 impl Robot {
     pub fn new(x: i32, y: i32, d: Direction) -> Self {
-        unimplemented!("Create a robot at (x, y) ({}, {}) facing {:?}", x, y, d,)
+        Self {
+            position: (x, y),
+            direction: d,
+        }
     }
 
     pub fn turn_right(self) -> Self {
-        unimplemented!()
+        let direction = match self.direction {
+            Direction::North => Direction::East,
+            Direction::East => Direction::South,
+            Direction::South => Direction::West,
+            Direction::West => Direction::North,
+        };
+
+        Self { direction, ..self }
     }
 
     pub fn turn_left(self) -> Self {
-        unimplemented!()
+        let direction = match self.direction {
+            Direction::North => Direction::West,
+            Direction::East => Direction::North,
+            Direction::South => Direction::East,
+            Direction::West => Direction::South,
+        };
+
+        Self { direction, ..self }
     }
 
     pub fn advance(self) -> Self {
-        unimplemented!()
+        let (mut x, mut y) = self.position;
+
+        match self.direction {
+            Direction::North => y += 1,
+            Direction::East => x += 1,
+            Direction::South => y -= 1,
+            Direction::West => x -= 1,
+        }
+
+        Self {
+            position: (x, y),
+            ..self
+        }
     }
 
     pub fn instructions(self, instructions: &str) -> Self {
-        unimplemented!(
-            "Follow the given sequence of instructions: {}",
-            instructions
-        )
+
+        let robot = instructions.chars().fold(self, |bot, c| 
+            match c {
+            'A' => return bot.advance(),
+            'L' => return bot.turn_left(),
+            'R' => return bot.turn_right(),
+            _ => panic!("a"),
+            }
+        );
+
+        robot
     }
 
     pub fn position(&self) -> (i32, i32) {
-        unimplemented!()
+        self.position
     }
 
     pub fn direction(&self) -> &Direction {
-        unimplemented!()
+        &self.direction
     }
 }
